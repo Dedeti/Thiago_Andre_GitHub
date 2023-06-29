973f8e8685c751913ed6d30a3f603a031eecb282
André Enes <andrenes2905@gmail.com>
2023-06-29 16:19:16 -0300

    Initial Commit

---
+++ Thiagao_Andre_GitHub/Thiagao_Andre_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thiagao_Andre_GitHubApp.swift
+//  Thiagao_Andre_GitHub
+//
+//  Created by André Enes Pecci on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Thiagao_Andre_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
